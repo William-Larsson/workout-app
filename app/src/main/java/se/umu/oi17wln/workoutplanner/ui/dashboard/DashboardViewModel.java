71e0a61ba907eadd552acd220fd131bd0aa6823b
William Larsson <williaamlarsson@gmail.com>
2020-07-21 14:24:19 +0200

    [feat] initial project setup

---
+++ app/src/main/java/se/umu/oi17wln/workoutplanner/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package se.umu.oi17wln.workoutplanner.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
